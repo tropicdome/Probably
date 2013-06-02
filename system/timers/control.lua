7d8a51bf546549ad4ce86d257585e10cd17a7846
Ben Phelps <bphelpsen@gmail.com>
2013-06-02 03:26:57 -0500

    had some debug stuff in that last commit

--- system/timers/control.lua
+++ system/timers/control.lua
@@ -1,10 +1,10 @@
 -- ProbablyEngine v0.0.1
 -- Ben Phelps (c) 2013
 
-ProbablyEngine.timer.register("HasFullControl", function()
+ProbablyEngine.timer.register("has_control", function()
   ProbablyEngine.module.player.control = HasFullControl() == 1
 end, 100)
 
-ProbablyEngine.timer.register("test", function()
-  CastSpellByName("Cobra Shot")
-end, 3000)
+ProbablyEngine.timer.register("moving", function()
+  ProbablyEngine.module.player.moving = GetUnitSpeed("player") ~= 0
+end, 100)
