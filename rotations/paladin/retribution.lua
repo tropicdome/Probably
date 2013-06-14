89d7e30c12adac880ef12fc286d3f1c78a250305
Ben Phelps <bphelpsen@gmail.com>
2013-06-13 21:13:09 -0500

    Retribution Paladin rotation.

---
+++ rotations/paladin/retribution.lua
@@ -0,0 +1,20 @@
+ProbablyEngine.rotation.register(66, {
+    { "Blessing of Kings", "!player.buff:Blessing of Kings" },
+    { "Avenging Wrath", "cooldowns" },
+    { "Inquisition", {
+        "!player.buff:Inquisition",
+        "holypower:3"
+    },
+    { "Templar's Verdict", "holypower:3" },
+    { "Hammer of Wrath ", "target.health:20" },
+    { "Hammer of Wrath ", "player.buff:Avenging Wrath" },
+    { "Exorcism" },
+    { "Avenger's Shield" },
+    { "Consecration" },
+    { "Holy Wrath" },
+    { "Hammer of Wrath" },
+    { "Hammer of the Righteous", "multitarget" },
+    { "Judgment" },
+    { "Crusader Strike" }
+  }
+)
