22cb2792fbfddfd58da1e8da5e9092791703b27c
Ben Phelps <bphelpsen@gmail.com>
2013-07-05 04:00:26 -0500

    Update prot pally to use new dsl.

--- rotations/paladin/protection.lua
+++ rotations/paladin/protection.lua
@@ -1,15 +1,15 @@
 ProbablyEngine.rotation.register(66, {
-    { "Blessing of Kings", "!player.buff:Blessing of Kings" },
+    { "Blessing of Kings", "!player.buff(Blessing of Kings)" },
     { "Avenging Wrath", "cooldowns" },
-    { "Sacred Shield", "!player.buff:Sacred shield" },
-    { "Hammer of the Righteous", "!target.debuff:Weakened Blows" },
+    { "Sacred Shield", "!player.buff(Sacred shield)" },
+    { "Hammer of the Righteous", "!target.debuff(Weakened Blows)" },
     { "Shield of the Righteous", {
-        "player.holypower:3",
-        "player.health:90"
+        "player.holypower = 3",
+        "player.health < 90"
     }},
     { "Word of Glory", {
-        "player.holypower:3",
-        "!player.health:90"
+        "player.holypower = 3",
+        "!player.health < 90"
     }},
     { "Avenger's Shield" },
     { "Consecration" },
