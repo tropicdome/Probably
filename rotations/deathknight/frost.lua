790920d8b2000f7f6ffaf8f8cff9d38b7b5b9bcd
Ben Phelps <bphelpsen@gmail.com>
2013-07-11 14:05:56 -0500

    Frost Death Knight
    Needs work.

--- rotations/deathknight/frost.lua
+++ rotations/deathknight/frost.lua
@@ -1,1 +1,18 @@
 -- SPEC ID 251
+ProbablyEngine.rotation.register(251, {
+
+    -- Keep up dots
+    { "Outbreak", "!target.debuff(Blood Plague)" },
+    { "Outbreak", "!target.debuff(Frost Fever)" },
+    { "Soul Reaper", "target.health < 30" },
+    { "Plague Strike", "!target.debuff(Blood Plague)" },
+    --{ "Howling Blast", "player.buff(Rime)" },
+    { "Obliterate", "player.buff(Killing Machine)" },
+    { "Frost Strike", "player.runicpower >= 80"},
+    { "Obliterate" },
+    --{ "Howling Blast", "player.buff(Freezing Fog)" },
+    { "Howling Blast", "player.runes(frost) = 2" },
+    { "Horn of Winter" },
+
+  }
+)
