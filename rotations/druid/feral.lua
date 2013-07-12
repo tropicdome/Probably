28a26198df4cdde762634d054a3aee0fa1fab32b
Ben Phelps <bphelpsen@gmail.com>
2013-07-12 08:59:34 -0500

    Feral DPS Rotation

--- rotations/druid/feral.lua
+++ rotations/druid/feral.lua
@@ -1,1 +1,34 @@
 -- SPEC ID 103
+ProbablyEngine.rotation.register(103, {
+    { "Faerie Fire", "!target.debuff(Faerie Fire)" },
+    { "Savage Roar", {
+      "!player.buff(Savage Roar)",
+      "player.combopoints = 0"
+    }},
+    { "Savage Roar", {
+      "player.buff(Savage Roar).duration < 3",
+      "player.combopoints = 5"
+    }},
+    { "Rip", {
+      "target.debuff(Rip).duration < 3",
+      "player.combopoints = 5"
+    }},
+    { "Mangle", "target.debuff(Infected Wounds).duration < 2" },
+    { "Tiger's Fury", "player.energy < 35"},
+    { "Ferocious Bite", {
+      "target.health <= 25",
+      "player.combopoints = 5"
+    }},
+    { "Rake", "target.debuff(Rake).duration < 3" },
+    { "Thrash", {
+      "player.buff(Omen of Clarity)",
+      "target.debuff(Thrash).duration < 3"
+    }},
+    { "Ferocious Bite", {
+      "player.combopoints = 5",
+      "target.debuff(Rip).duration < 6",
+      "player.buff(Savage Road).duration < 6",
+    }},
+    { "Shred", "player.combopoints < 5" }
+  }
+)
