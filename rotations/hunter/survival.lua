657a7be2d3df78d67a9ce5f0bfee5ecdadf1667b
Ben Phelps <bphelpsen@gmail.com>
2013-07-12 14:19:00 -0500

    Add multi target to Survival Hunter rotation.

--- rotations/hunter/survival.lua
+++ rotations/hunter/survival.lua
@@ -3,6 +3,14 @@ ProbablyEngine.rotation.register(255, {
     { "Snake Trap"    , "modifier.shift", "ground" },
     { "Explosive Trap", "modifier.shift", "ground" },
     { "Freezing Trap" , "modifier.shift", "ground" },
+    { "Multi-Shot", {
+        "player.buff(Thrill of the Hunt)",
+        "modifier.multitarget"
+    }},
+    { "Multi-Shot", {
+        "player.focus >= 60",
+        "modifier.multitarget"
+    }},
     { "Arcane Shot", "player.buff(Thrill of the Hunt)"},
     { "Arcane Shot", "player.focus >= 60"},
     { "Black Arrow"},
