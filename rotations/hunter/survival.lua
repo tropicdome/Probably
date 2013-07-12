fb6915e19643353d1bcdc4fee06205dff0ee0fc3
Ben Phelps <bphelpsen@gmail.com>
2013-07-12 11:30:53 -0500

    Add traps to Survival Hunter

--- rotations/hunter/survival.lua
+++ rotations/hunter/survival.lua
@@ -1,5 +1,8 @@
 -- SPEC ID 255
 ProbablyEngine.rotation.register(255, {
+    { "Snake Trap"    , "modifier.shift", "ground" },
+    { "Explosive Trap", "modifier.shift", "ground" },
+    { "Freezing Trap" , "modifier.shift", "ground" },
     { "Arcane Shot", "player.buff(Thrill of the Hunt)"},
     { "Arcane Shot", "player.focus >= 60"},
     { "Black Arrow"},
