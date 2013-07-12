ce711b311a95243cce69bcc119ac95766b76915a
Ben Phelps <bphelpsen@gmail.com>
2013-07-12 18:57:13 -0500

    Marksmanship hunter rotation.

--- rotations/hunter/marksmanship.lua
+++ rotations/hunter/marksmanship.lua
@@ -1,1 +1,55 @@
 -- SPEC ID 254
+ProbablyEngine.rotation.register(254, {
+
+    -- Traps
+    { "Snake Trap"    , "modifier.shift", "ground" },
+    { "Explosive Trap", "modifier.alt", "ground" },
+    { "Freezing Trap" , "modifier.shift", "ground" },
+
+    -- Cooldowns
+    { "Stampede", "modifier.cooldowns" },
+    { "A Murder of Crows", "modifier.cooldowns" },
+    { "Lynx Rush", "modifier.cooldowns" },
+    { "Rapid Fire", "modifier.cooldowns" },
+    { "Rabid", {
+        "modifier.cooldowns",
+        "player.buff(Rapid Fire)"
+    }},
+
+    -- AoE
+    { "Multi-Shot", {
+        "player.buff(Thrill of the Hunt)",
+        "modifier.multitarget"
+    }},
+    { "Multi-Shot", {
+        "player.focus >= 60",
+        "modifier.multitarget"
+    }},
+
+    -- Debuffs
+    { "Serpent Sting", "!target.debuff(Serpent Sting)" },
+
+    -- Careful Aim Rotation ( 90% + HP )
+    {{
+      { "Chimera Shot", "target.debuff(Serpent Sting).duration < 5" },
+      { "Aimed Shot", "!player.moving" },
+      { "Aimed Shot", "player.buff(Fire!)" },
+      { "Steady Shot", "player.debuff(Steady Focus).duration < 6" },
+      { "Steady Shot" }
+    }, "target.health > 80" },
+
+    -- Standard Rotation ( 90% - HP )
+    {{
+      { "Serpent Sting", "!target.debuff(Serpent Sting)" },
+      { "Glaive Toss" },
+      { "Chimera Shot" },
+      { "Kill Shot" },
+      { "Dire Beast" },
+      { "Fervor" },
+      { "Aimed Shot", "player.buff(Fire!)" },
+      { "Arcane Shot", "player.focus >= 65" },
+      { "Steady Shot" }
+    }, "target.health < 80"}
+
+  }
+)
