761c5bacc3b0d018643fc6381d72bec9441b6499
Ben Phelps <bphelpsen@gmail.com>
2013-07-12 14:00:23 -0500

    Set toggle button to spec or spell icon

--- system/listeners/player_specialization_changed.lua
+++ system/listeners/player_specialization_changed.lua
@@ -7,6 +7,7 @@ ProbablyEngine.listener.register("PLAYER_SPECIALIZATION_CHANGED", function(...)
   if unitID == "player" then
     local id, name, description, icon, background, role = GetSpecializationInfo(GetSpecialization());
     if id ~= ProbablyEngine.module.player.specId then
+      ProbablyEngine.buttons.icon('MasterToggle', icon)
       ProbablyEngine.module.player.specId = id
       ProbablyEngine.module.player.specName = name
       ProbablyEngine.print(ProbablyEngine.rotation.specId[id] .. " rotation loaded!")
