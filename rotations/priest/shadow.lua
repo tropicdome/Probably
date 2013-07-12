8c8eef854e5e4a55a0ea9b4aa7d4b17b42c59a4a
Jokur <jokur@pocketgno.me>
2013-07-12 12:32:32 -0500

    COmmitted First Changes
    None

--- rotations/priest/shadow.lua
+++ rotations/priest/shadow.lua
@@ -7,11 +7,17 @@ ProbablyEngine.rotation.register(258, {
     { "Inner Fire", "!player.buff(Inner Fire)" },
     { "Shadow Form", "!player.buff(Shadowform)" },
     
+    -- We are moving
+    { "Shadow Word: Pain", "player.moving" },
+    { "Cascade", "player.moving" },
+    { "Halo", "player.moving" },
+    { "Shadow Word: Death", "player.moving" },
     -- Main DPS 
     { "Mind Blast", "player.buff(Divine Insight)" },
     { "Devouring Plague", "player.shadoworbs = 3" },
 
     { "Mind Blast" }, 
+    { "Shadow Word: Death", "target.debuff(Shadow Word: Death).duration < 1" },
     { "Shadow Word: Pain", "target.debuff(Shadow Word: Pain).duration < 1" },
     { "Vampiric Touch", "target.debuff(Vampiric Touch).duration < 1" },
     { "Mind Flay", "target.debuff(Devouring Plague)" }, 
