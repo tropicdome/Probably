4fe7830a5258b0d7a54b5611ccdb3f87c7a6a48b
Jokur <jokur@pocketgno.me>
2013-07-12 12:10:19 -0500

    Priest Updated

--- rotations/priest/shadow.lua
+++ rotations/priest/shadow.lua
@@ -1,15 +1,31 @@
 -- SPEC ID 258
 ProbablyEngine.rotation.register(258, {
+
+	-- Maintain these buffs
     { "Power Word: Fortitude", "!player.buff(Power Word: Fortitude)" },
     { "Inner Fire", "!player.buff(Inner Fire)" },
     { "Shadow Form", "!player.buff(Shadowform)" },
-    { "Shadow Word: Pain", "target.debuff(Shadow Word: Pain).duration < 3" },
-    { "Vampiric Touch", "target.debuff(Vampiric Touch).duration < 4" },
+    
+    -- Main DPS 
+    { "Mind Blast", "player.buff(Divine Insight)" },
     { "Devouring Plague", "player.shadoworbs = 3" },
-    { "Mind Blast" },
+
+    { "Mind Blast" }, 
+    { "Shadow Word: Pain", "target.debuff(Shadow Word: Pain).duration < 1" },
+    { "Vampiric Touch", "target.debuff(Vampiric Touch).duration < 1" },
+    { "Mind Flay", "target.debuff(Devouring Plague)" }, 
+    { "Cascade", },
+    { "Halo" },
     { "Mind Spike", "player.buff(Surge of Darkness)" },
+    { "Mind Flay" },
+ 
+    
     { "Shadow Word: Death" },
-    { "Mind Flay", "target.debuff(Devouring Plague)" },
-    { "Mind Flay" }
+    
+    
+   
+    
+    
+    
   }
 )
