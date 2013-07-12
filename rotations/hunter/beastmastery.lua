426e0d96a674707225536a566b92e4947b281968
Ben Phelps <bphelpsen@gmail.com>
2013-07-12 08:59:27 -0500

    Add traps to BM hunter.

--- rotations/hunter/beastmastery.lua
+++ rotations/hunter/beastmastery.lua
@@ -1,6 +1,11 @@
 -- SPEC ID 253
 ProbablyEngine.rotation.register(253, {
     -- { "Hunter's Mark" , "!target.debuff(Hunter's Mark)" },
+
+    { "Snake Trap"    , "modifier.shift", "ground" },
+    { "Explosive Trap", "modifier.shift", "ground" },
+    { "Freezing Trap" , "modifier.shift", "ground" },
+    
     { "Serpent Sting", "!target.debuff(Serpent Sting)" },
     { "Focus Fire", "player.buff(Frenzy).count = 5" },
     { "Kill Command" },
