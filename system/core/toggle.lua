f65eb7ba162b04f4f7251b876b42b977df86b93e
Ben Phelps <bphelpsen@gmail.com>
2013-07-13 13:35:58 -0500

    Add abstraction to toggle buttons

--- system/core/toggle.lua
+++ system/core/toggle.lua
@@ -2,5 +2,15 @@
 -- Ben Phelps (c) 2013
 
 ProbablyEngine.toggle = {
-
+  states = { }
 }
+
+ProbablyEngine.toggle.create = function(toggle_name, icon, tooltip)
+  ProbablyEngine.toggle.states[toggle_name] = false
+  local toggleCallback = function(self)
+    ProbablyEngine.toggle.states[toggle_name] = not ProbablyEngine.toggle.states[toggle_name]
+    self.checked = not self.checked
+    self:SetChecked(self.checked)
+  end
+  ProbablyEngine.buttons.create(toggle_name, icon, toggleCallback, tooltip)
+end
