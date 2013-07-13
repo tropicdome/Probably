43d56f0ff2cad892c0f2d46b681ad24ff5bb9cf5
Ben Phelps <bphelpsen@gmail.com>
2013-07-13 13:12:36 -0500

    Clear Arms Rotation

--- rotations/warrior/arms.lua
+++ rotations/warrior/arms.lua
@@ -1,30 +1,5 @@
 -- SPEC ID 71
 ProbablyEngine.rotation.register(71, {
-  { "Battle Shout" },
-  { "Recklessness", {
-      "cooldowns",
-      "target.health:20",
-      "target.debuff.duration(5):Colossus Smash"
-  }},
-  { "Berserker Rage", "cooldowns" },
-  { "Deadly Calm", {
-      "cooldowns",
-      "player.rage:40"
-  }},
-  { "Lifeblood", "cooldowns" },
-  { "Impending Victory", "player.health:90" },
-  { "Heroic Strike", {
-      "target.debuff:Colossus Smash",
-      "player.rage:70"
-  }},
-  { "Dragon Roar" },
-  { "Mortal Strike" },
-  { "Sweeping Strikes", "multitarget"},
-  { "Colossus Smash", "target.debuff.duration(1.5):Colossus Smash" },
-  { "Execute" },
-  { "Overpower" },
-  { "Slam", {
-      "player.rage:40",
-      "target.health:20"
-  }}
+
+
 })
