5de5b8e1eb689b4fab89c6dc84cc885c043209a9
Ben Phelps <bphelpsen@gmail.com>
2013-07-13 19:12:05 -0500

    Add Arms Warrior rotation

--- rotations/warrior/arms.lua
+++ rotations/warrior/arms.lua
@@ -1,5 +1,46 @@
 -- SPEC ID 71
 ProbablyEngine.rotation.register(71, {
 
+  -- Buffs
+  { "Berserker Rage" },
+
+  -- Survival
+  { "Rallying Cry", {
+    "player.health < 15",
+    "modifier.cooldowns"
+  }},
+
+  { "Die by the Sword", "player.health < 25" },
+  { "Impending Victory" },
+  { "Victory Rush" },
+
+  -- Kicks
+  { "Pummel", "modifier.interrupts" },
+  { "Disrupting Shout", "modifier.interrupts" },
+
+  -- Cooldowns
+  { "Bloodbath", "modifier.cooldowns" },
+  { "Avatar", "modifier.cooldowns" },
+  { "Recklessness", "modifier.cooldowns" },
+  { "Skull Banner", "modifier.cooldowns" },
+  { "Bladestorm", "modifier.cooldowns" },
+
+  -- AoE
+  { "Sweeping Strikes", "modifier.multitarget" },
+  { "Thunder Clap", "modifier.multitarget" },
+  { "Whirlwind", "modifier.multitarget" },
+  { "Dragon Roar", "modifier.multitarget" },
+
+  -- Rotation
+  { "Overpower" },
+  { "Mortal Strike" },
+  { "Colossus Smash" },
+  { "Heroic Throw" },
+  { "Heroic Strike", {
+    "player.rage > 70",
+    "target.debuff(Colossus Smash)"
+  }},
+  { "Slam", "player.rage > 40" },
+  { "Execute" },
 
 })
