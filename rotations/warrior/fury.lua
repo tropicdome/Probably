64496adf2ffbeec8160824f90c9fe9a4fe798e15
Ben Phelps <bphelpsen@gmail.com>
2013-07-13 19:11:52 -0500

    Add Fury Warrior rotation

--- rotations/warrior/fury.lua
+++ rotations/warrior/fury.lua
@@ -1,1 +1,43 @@
 -- SPEC ID 72
+ProbablyEngine.rotation.register(72, {
+
+  -- Buffs
+  { "Berserker Rage" },
+
+  -- Survival
+  { "Rallying Cry", {
+    "player.health < 15",
+    "modifier.cooldowns"
+  }},
+
+  { "Die by the Sword", "player.health < 25" },
+  { "Impending Victory" },
+  { "Victory Rush" },
+
+  -- Kicks
+  { "Pummel", "modifier.interrupts" },
+  { "Disrupting Shout", "modifier.interrupts" },
+
+  -- Cooldowns
+  { "Bloodbath", "modifier.cooldowns" },
+  { "Avatar", "modifier.cooldowns" },
+  { "Recklessness", "modifier.cooldowns" },
+  { "Skull Banner", "modifier.cooldowns" },
+  { "Bladestorm", "modifier.cooldowns" },
+
+  -- AoE
+  { "Thunder Clap", "modifier.multitarget" },
+  { "Whirlwind", "modifier.multitarget" },
+  { "Dragon Roar", "modifier.multitarget" },
+
+  -- Rotation
+  { "Colossus Smash" },
+  { "Bloodthirst" },
+  { "Execute", "player.rage > 80" },
+  { "Heroic Strike", "player.rage > 80" },
+  { "Raging Blow" },
+  { "Wild Strike", "player.buff(Bloodsurge)" },
+  { "Impending Victory" },
+  { "Heroic Throw" }
+
+})
