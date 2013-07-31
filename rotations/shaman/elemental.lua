01c7f0a08e312bda240afe25c18c97ada524246c
Ben Phelps <bphelpsen@gmail.com>
2013-07-30 19:27:25 -0500

    Shaman Rotations

--- rotations/shaman/elemental.lua
+++ rotations/shaman/elemental.lua
@@ -1,1 +1,42 @@
 -- SPEC ID 262
+ProbablyEngine.rotation.register(262, {
+
+    -- Buffs
+    { "Flametongue Weapon", "!enchant.mainhand" },
+    { "Lightning Shield", "!player.buff(Lightning Shield)" },
+
+    -- Moving Rotation
+    { "Lightning Bolt", {
+      "player.moving",
+      "!player.buff(Spiritwalker's Grace)"
+    }},
+
+    -- Cooldowns
+    { "Shamanistic Rage", "modifier.cooldowns" },
+    { "Stormlash Totem", "modifier.cooldowns" },
+    { "Fire Elemental Totem", "modifier.cooldowns" },
+    { "Ascendance", {
+      "modifier.cooldowns",
+      "!player.buff(Ascendance)"
+    }},
+
+    -- Totems
+    { "Searing Totem", {
+      "!totem(Fire Elemental Totem)",
+      "!totem(Searing Totem)"
+    }},
+    { "Healing Stream", "!totem(Healing Stream)" },
+
+    -- Rotation
+    { "Flame Shock", "target.debuff(Flame Shock).duration <= 3" },
+    { "Lava Burst" },
+    { "Elemental Blast" },
+    { "Earth Shock", "player.buff(Lightning Shield).count >= 6" },
+
+    -- Aoe
+    { "Chain Lightning", "modifier.multitarget" },
+
+    -- Filler
+    { "Lightning Bolt" }
+  }
+)
--- rotations/shaman/enhancement.lua
+++ rotations/shaman/enhancement.lua
@@ -1,1 +1,43 @@
 -- SPEC ID 263
+ProbablyEngine.rotation.register(263, {
+
+    -- Buffs
+    { "Windfury Weapon", "!enchant.mainhand" },
+    { "Flametongue Weapon", "!enchant.offhand" },
+    { "Lightning Shield", "!player.buff(Lightning Shield)" },
+
+    -- Cooldowns
+    { "Fire Elemental Totem", "modifier.cooldowns" },
+    { "Earth Elemental Totem", "modifier.cooldowns" },
+    { "Feral Spirit", "modifier.cooldowns" },
+    { "Stormlash Totem", "modifier.cooldowns" },
+    { "Ascendance", {
+      "modifier.cooldowns",
+      "!player.buff(Ascendance)"
+    }},
+
+    -- Totems
+    { "Searing Totem", {
+      "!totem(Fire Elemental Totem)",
+      "!totem(Searing Totem)"
+    }},
+
+    -- AoE
+    { "Chain Lightning", {
+      "player.buff(Maelstrom Weapon).count = 5",
+      "modifier.multitarget"
+    }},
+    { "Fire Nova", {
+      "modifier.multitarget",
+      "target.debuff(Flame Shock)"
+    }},
+
+    -- Rotation
+    { "Lightning Bolt", "player.buff(Maelstrom Weapon).count = 5" },
+    { "Stormstrike" },
+    { "Stormblast" },
+    { "Flame Shock", "target.debuff(Flame Shock).duration <= 3" },
+    { "Lava Lash" },
+    { "Unleash Elements" },
+    { "Earth Shock" },
+})
