cd5acca636424387ced53ed3ec78b39e25457a79
Ben Phelps <bphelpsen@gmail.com>
2013-08-07 19:23:03 -0500

    Rune of Power so we don't oom.

--- rotations/mage/arcane.lua
+++ rotations/mage/arcane.lua
@@ -1,6 +1,7 @@
 -- SPEC ID 62
 ProbablyEngine.rotation.register(62, {
     { "Frost Armor", "!player.buff(Frost Armor)" },
+    { "Rune of Power", "modifier.shift", "ground" },
     { "Arcane Brilliance", "!player.buff(Arcane Brilliance)" },
     { "Nether Tempest",  "!target.debuff(Nether Tempest)" },
     { "Ice Lance", "player.moving" },
