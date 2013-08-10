4737b781eab4ee7a276d37acafbe701138d05ba0
Ben Phelps <bphelpsen@gmail.com>
2013-08-10 03:08:39 -0500

    Unholy DK rotation

--- rotations/deathknight/unholy.lua
+++ rotations/deathknight/unholy.lua
@@ -1,1 +1,25 @@
 -- SPEC ID 252
+ProbablyEngine.rotation.register(252, {
+
+  -- Cooldowns
+  { "Unholy Frenzy", "modifier.cooldowns" },
+  { "Summon Gargoyle", "modifier.cooldowns" },
+
+  -- Survival
+  { "Death Pact", "player.health < 20" },
+  { "Icebound Fortitude", "player.health < 50" },
+
+  -- Rotation
+  { "Outbreak", "!target.debuff(Frost Fever)" },
+  { "Icy Touch", "target.debuff(Frost Fever).duration < 4" },
+  { "Plague Strike", "target.debuff(Blood Plague).duration < 4" },
+  { "Death and Decay", "modifier.shift" },
+  { "Soul Reaper", "target.health < 35" },
+  { "Dark Transformation" },
+  { "Death Coil", "player.buff(Sudden Doom)" },
+  { "Scourge Strike" },
+  { "Festering Strike" },
+  { "Death Coil", "player.runicpower > 90"},
+  { "Horn of Winter" },
+
+})
