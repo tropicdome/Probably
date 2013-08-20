f85f404641acff0c563c6fb436e3a683a8ad657d
Ben Phelps <bphelpsen@gmail.com>
2013-08-20 08:59:35 -0500

    Allow macros to be queued

--- system/modules/queue.lua
+++ system/modules/queue.lua
@@ -2,7 +2,8 @@
 -- Ben Phelps (c) 2013
 
 ProbablyEngine.module.register("queue", {
-  queue = nil
+  queue = nil,
+  macro_queue = nil
 })
 
 ProbablyEngine.module.queue.add_spell = function(spell_name)
@@ -12,17 +13,28 @@ ProbablyEngine.module.queue.add_spell = function(spell_name)
   end
 end
 
+ProbablyEngine.module.queue.add_macro = function(macro_id)
+  if ProbablyEngine.module.queue.macro_queue ~= macro_id then
+    ProbablyEngine.module.queue.macro_queue = macro_id
+    ProbablyEngine.debug("Macro ID " .. macro_id .. " was queued for casting.", 2)
+  end
+end
+
 -- This hooks the secure function "UseAction" that is called when we press a button on our
 -- action bars.  This allows us to let the engine know we would like to cast a spell other
 -- than the one currently active in the rotation.
 
 hooksecurefunc("UseAction", function(...)
-  if ProbablyEngine.module.player.combat then
+  if ProbablyEngine.module.player.combat and UnitAffectingCombat("player") then
     if select(3, ...) ~= nil then
       local type, id, subType, spellID = GetActionInfo(select(1,...))
       if type == "spell" then
         local name,_,_,_,_,_,_,_,_ = GetSpellInfo(id)
-        ProbablyEngine.module.queue.add_spell(name)
+        if ProbablyEngine.parser.can_cast_queue(name) then
+          ProbablyEngine.module.queue.add_spell(name)
+        end
+      elseif type == "macro" then
+        ProbablyEngine.module.queue.add_macro(id)
       end
     end
   end
