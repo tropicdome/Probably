ba968a43af5774a5ce430bc01d0e795257f227dd
Ben Phelps <bphelpsen@gmail.com>
2013-08-20 08:59:15 -0500

    Improved cycle time.

--- system/timers/lag.lua
+++ system/timers/lag.lua
@@ -4,7 +4,7 @@ local warningSent = false
 
 ProbablyEngine.timer.register("lag", function()
   local bandwidthIn, bandwidthOut, latencyHome, latencyWorld = GetNetStats()
-  ProbablyEngine.lag = (((latencyWorld + latencyHome) / 2) / 2)
+  ProbablyEngine.lag = math.floor( (((latencyWorld + latencyHome) / 2) / 2) )
   -- Dynamic rotation timing
   if ProbablyEngine.dynamicCycle == true then
     if ProbablyEngine.lag < 500 then
@@ -12,6 +12,8 @@ ProbablyEngine.timer.register("lag", function()
       ProbablyEngine.timer.updatePeriod("rotation", ProbablyEngine.cycleTime)
       ProbablyEngine.debug("Dynamic Cycle Update: " .. ProbablyEngine.cycleTime .. "ms" , 4)
     end
+  else
+    ProbablyEngine.cycleTime = ProbablyEngine_Data.cycle_time
   end
 end, 2000)
 
