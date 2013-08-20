e7bb6974ca0b757530aff32b9ed34aa389238595
Ben Phelps <bphelpsen@gmail.com>
2013-08-20 09:00:11 -0500

    NYU

--- system/core/spellcache.lua
+++ system/core/spellcache.lua
@@ -1,26 +1,21 @@
 -- ProbablyEngine v0.0.1
 -- Ben Phelps (c) 2013
 
--- You see this a lot but is is never explained so here we go!
+ProbablyEngine.spellcache = {
+  tmp = { }
+}
 
--- Create a meta table to hold a cache of spell info
-local spellcache = setmetatable( {},
+ProbablyEngine.spellcache.cache = setmetatable( {},
 {
-  -- the __index is a magic method that is used as a fallback if the table does not have the
-  -- key we asked for, this is the core of how it helps speed up calls to GetSpellInfo
-  __index = function (t,v)
-    local a = {GetSpellInfo(v)}
-    -- we cache the spell call here
-    if GetSpellInfo(v) then
-      t[v] = a
+  __index = function (table, value)
+    local tmp = {GetSpellInfo(value)}
+    if GetSpellInfo(value) then
+      table[value] = tmp
     end
-    return a
+    return tmp
   end
 })
 
--- Herer we overwrite the default GetSpellInfo
--- to use our newly created metatable
-local function GetSpellInfo(a)
-    -- we just unpack the table entry if one exists
-    return unpack(spellcache[a])
+ProbablyEngine.spellcache.gsi = function(spell)
+    return unpack(ProbablyEngine.spellcache.cache[spell])
 end
