fddf67e42294df8bf10924b905c835cb8975878c
Ben Phelps <bphelpsen@gmail.com>
2013-10-13 16:03:29 -0500

    Spell cache broken for now

--- system/core/spellcache.lua
+++ system/core/spellcache.lua
@@ -2,21 +2,5 @@
 -- Ben Phelps (c) 2013
 
 local GetSpellInfo = GetSpellInfo
-
-ProbablyEngine.gsi = {
-
-}
-
-ProbablyEngine.gsi.cache = setmetatable(
-  { },
-  {__index = function(t, v)
-      local a = { GetSpellInfo(v) }
-      if GetSpellInfo(v)
-        then t[v] = a
-      end
-      return a
-    end
-  }
-)
-
-ProbablyEngine.gsi.call = function(a) return unpack(ProbablyEngine.gsi.cache[a]) end
+ProbablyEngine.gsi = { }
+ProbablyEngine.gsi.call = GetSpellInfo
