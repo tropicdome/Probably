5e95cce7606f917062336b958e1c1cbf8c9c7d41
Ben Phelps <bphelpsen@gmail.com>
2013-10-27 16:24:51 -0500

    Rename slash to command

---
+++ system/core/command.lua
@@ -0,0 +1,40 @@
+-- ProbablyEngine Rotations - https://probablyengine.com/
+-- Released under modified BSD, see attached LICENSE.
+
+ProbablyEngine.command = {
+  commands = 0,
+  handlers = { }
+}
+
+ProbablyEngine.command.print = function(message)
+  print("|cFF"..ProbablyEngine.addonColor.."["..ProbablyEngine.addonName.."]|r " .. message .. "")
+end
+
+ProbablyEngine.command.register = function (command, handler)
+  local name = 'PE_' .. command
+  _G["SLASH_" .. name .. "1"] = '/' .. command
+  SlashCmdList[name] = function(message, editbox) handler(message, editbox) end
+end
+
+ProbablyEngine.command.register('pe', function(msg, box)
+  local command, text = msg:match("^(%S*)%s*(.-)$")
+  if ProbablyEngine.command.handlers[command] then
+    ProbablyEngine.command.handlers[command](text)
+  else
+    ProbablyEngine.command.print('Unknown command: ' .. command);
+  end
+end)
+
+ProbablyEngine.command.register_handler = function(command, handler)
+  local command_type = type(command)
+  if command_type == "string" then
+    ProbablyEngine.command.handlers[command] = handler
+  elseif command_type == "table" then
+    for _,com in pairs(command) do
+      ProbablyEngine.command.handlers[com] = handler
+    end
+  else
+    ProbablyEngine.command.print('Unknown Command Type: ' .. command_type)
+  end
+end
+
