e2eef28bbddd265df4a033125e9945cc4793f8c2
Ben Phelps <bphelpsen@gmail.com>
2013-11-01 22:25:59 -0500

    Completely rewritten listener core

--- system/listeners/player_login.lua
+++ system/listeners/player_login.lua
@@ -3,7 +3,7 @@
 
 ProbablyEngine.listener.register("PLAYER_LOGIN", function(...)
   ProbablyEngine.rotation.auto_unregister()
-  ProbablyEngine.listener.eventActions["PLAYER_SPECIALIZATION_CHANGED"]("player")
+  ProbablyEngine.listener.trigger("PLAYER_SPECIALIZATION_CHANGED", "player")
   ProbablyEngine.rotation.add_buttons()
   if not ProbablyEngine.full then
     ProbablyEngine.full_check()
